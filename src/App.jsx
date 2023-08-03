800e35fbcfa8a8f0824eaef6a2cf88a8a8032bd0
KaylaTan91 <kaylatan91@gmail.com>
2023-08-03 16:46:30 -0400

    Add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList", puppyList);
+  return (
+    <div className="App">
+      {
+         puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+      }
+  </div>
+  );
 }
 
 export default App
