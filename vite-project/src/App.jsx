f1f912a4106d6e8af6ca52c30a8cdb1126d1321c
Kirby S <130011561+schuetzk@users.noreply.github.com>
2023-08-06 22:37:19 -0700

    final changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,40 +1,34 @@
-import './App.css'
-import './Index.css'
 import { useState } from 'react'
 import {puppyList} from './data.js'
-
+import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  const [featPupId, setFeatPupId] = useState(null)
+  const [featPupId, setFeatPupId] = useState(null);
   
   function handleClick() {
   }
+  
   const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   console.log(featuredPup);
-
   return(
       <div className="App">
-        { 
-          puppies.map((puppy) => {
-            return <p key={puppy.id} onClick={()=>{ setFeatPupId(puppy.id)}}>
-              {puppy.name}
-            </p>;
-   })
-}
-    <p>{featPupId && (
-        <div>
+        {puppies.map((puppy) => {
+            return ( <p className="card" onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}
+            </p>
+        )
+   })}
+    {featPupId && (
+        <div className="Info">
           <h2>{featuredPup.name}</h2>
           <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
+            <li className="Info">Age: {featuredPup.age}</li>
+            <li className="Info">Email: {featuredPup.email}</li>
           </ul>
         </div>
       )}
-  </p>
-
 </div>
-  );
+  )
 }
 
 export default App
